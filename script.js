e82662faa3ba4b6e9cae0d0e3b5500ccca5468b5
tonyystaaark <144714043+tonyystaaark@users.noreply.github.com>
2025-12-06 18:59:24 +0800

    About me correction.

--- script.js
+++ script.js
@@ -3,45 +3,30 @@ document.addEventListener('DOMContentLoaded', function() {
     const navBar = document.getElementById('navBar');
     const hamburger = document.getElementById('hamburger');
     const navMobileMenu = document.getElementById('nav-menu');
+
     // Modal functionality
+    const heroSection = document.getElementById("heroSection");
     const aboutModal = document.getElementById('about-modal');
     const contactLink = document.getElementById('navContact');
     const modelPortrait = document.getElementById('model-portrait');
     const closeSign = document.getElementById('close-sign');
-    const cover = document.getElementById('cover');
+    const modalCover = document.getElementById('modalCover');
 
-    // hamburger
-    function toggleMenu() {
-        console.log("hamburger clicked");
-        navMobileMenu.classList.toggle('active');
-        hamburger.classList.toggle('active');
-    }
     hamburger.addEventListener('click', toggleMenu);
 
-    function showAboutModal() {
-        modelPortrait.classList.toggle('active');
-    }
     // show/hide modal
     contactLink.addEventListener('click', ()=> {
         console.log("contact link clicked");
         closeMenu();
         showAboutModal();
-        showCover();
+        showModalCover();
     });
-
-    // show/hide navBar
-    function closeMenu() {
-    if (navMobileMenu.classList.contains('active')) {
-        navMobileMenu.classList.remove('active');
-        hamburger.classList.remove('active');
-        }
-    }
 
     closeSign.addEventListener('click', function(){
         console.log("closeSign clicked");
         closeMenu();
         showAboutModal();
-        showCover();
+        showModalCover();
     })
 
     document.addEventListener('click', (event) => {
@@ -55,9 +40,40 @@ document.addEventListener('DOMContentLoaded', function() {
             closeMenu();
         }
     });
-    // cover .visibilityState
-    function showCover(){
-        console.log('show cover clicked')
-            cover.classList.toggle('active');
+    
+//----------------FUNCTIONS-----------------------------
+        // cover .visibilityState
+        function showModalCover(){
+            console.log('show cover clicked')
+                modalCover.classList.toggle('active');
         }
+
+        // show/hide navBar
+        function closeMenu() {
+        if (navMobileMenu.classList.contains('active')) {
+            navMobileMenu.classList.remove('active');
+            hamburger.classList.remove('active');
+            }
+        }
+
+        // hamburger
+        function toggleMenu() {
+            console.log("hamburger clicked");
+            navMobileMenu.classList.toggle('active');
+            hamburger.classList.toggle('active');
+        }
+        function showAboutModal() {
+            modelPortrait.classList.toggle('active');
+        }
+    
+// ------------------------------------------------------------
+        
+        const bodyClicked = document.body;
+
+        bodyClicked.addEventListener('click', function(){
+            console.log("body clicked");
+        })
+
+
+
 });
